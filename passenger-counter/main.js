21cdf7bf27ed3fcb89e6cb9a088d99ad3bad8db5
Glory Praise Emmanuel <emmaglorypraise@gmail.com>
2021-06-26 16:04:11 -0700

    summed total passengers

--- passenger-counter/main.js
+++ passenger-counter/main.js
@@ -1,12 +1,36 @@
+// Connects HTML DOM
 let countEl = document.getElementById('count-el');
+let saveEl = document.getElementById('save-el');
+let totalEl = document.getElementById('total-el');
+
+//new variables to hold number of people
 let count = 0;
 
+// stores the old values
+let newCount = 0;
+
+// triggered when increment button is clicked and changes initial value
 function increment() {
-    count = count + 1 ;
-    countEl.innerHTML = count;
-    console.log('count')
+    count +=  1 ;
+    countEl.textContent = count;
 }
 
+// triggered when save button is clicked
 function save() {
-    console.log(count);
+    
+    let countStr = count + ' + ';
+    
+
+    let totalEnteries;
+    newCount += count;
+    totalEnteries = newCount;
+
+    totalEl.textContent = totalEnteries
+    saveEl.textContent += countStr;
+
+    // resets when save button is clicked
+    countEl.textContent = 0;
+    count = 0;
+
 }
+
